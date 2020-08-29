dde6b9acec5542b4fe40380b31ef2e9d1b4ac51a
gilmarxd <gilmar_custodio_@hotmail.com>
2020-08-29 11:38:31 -0300

    [front-end]: create authentication middleware

--- front-end/src/middlewares/api.ts
+++ front-end/src/middlewares/api.ts
@@ -1,5 +1,14 @@
 import axios from 'axios'
+import { getToken } from './auth'
 
 const api = axios.create({ baseURL: 'http://192.168.1.2:3333' })
+
+api.interceptors.request.use(async (config) => {
+	const token = getToken()
+
+	if (token) config.headers.Authorization = `Bearer ${token}`
+
+	return config
+})
 
 export default api
