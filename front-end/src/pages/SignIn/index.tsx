0de857b28028458f515a9d616d0a29653679511b
gilmarxd <gilmar_custodio_@hotmail.com>
2020-08-29 11:43:12 -0300

    [front-end]: remove duplicated code and separate styles from pages

--- front-end/src/pages/SignIn/index.tsx
+++ front-end/src/pages/SignIn/index.tsx
@@ -1,54 +1,27 @@
 import React from 'react'
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import TextField from '@material-ui/core/TextField'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
-import Link from '@material-ui/core/Link'
-import Grid from '@material-ui/core/Grid'
-import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
+import {
+	Avatar,
+	Button,
+	Checkbox,
+	CssBaseline,
+	TextField,
+	FormControlLabel,
+	Link,
+	Grid,
+	Box,
+	Container,
+} from '@material-ui/core'
+
 import { FiUser } from 'react-icons/fi'
 
 import { Formik, Form, Field } from 'formik'
 import { object, string } from 'yup'
+
+import useStyles from './styles'
+
 import api from '../../middlewares/api'
-
-function Copyright() {
-	return (
-		<Typography variant='body2' color='textSecondary' align='center'>
-			{'Copyright © '}
-			<Link color='inherit' href='https://material-ui.com/'>
-				Your Website
-			</Link>{' '}
-			{new Date().getFullYear()}
-			{'.'}
-		</Typography>
-	)
-}
-
-const useStyles = makeStyles((theme) => ({
-	paper: {
-		marginTop: theme.spacing(8),
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-	},
-	avatar: {
-		margin: theme.spacing(1),
-		backgroundColor: theme.palette.grey[500],
-	},
-	form: {
-		width: '100%', // Fix IE 11 issue.
-		marginTop: theme.spacing(1),
-	},
-	submit: {
-		margin: theme.spacing(3, 0, 2),
-	},
-}))
+import { login } from '../../middlewares/auth'
+import Copyright from '../../components/Copyright'
 
 const SignIn = () => {
 	const classes = useStyles()
@@ -81,6 +54,7 @@ const SignIn = () => {
 								actions.setErrors(response.data.errors)
 							} else {
 								localStorage.setItem('id', response.data.id)
+								login(response.data)
 								window.location.href = '/'
 							}
 						})
--- front-end/src/pages/SignUp/index.tsx
+++ front-end/src/pages/SignUp/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import TextField from '@material-ui/core/TextField'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
-import Link from '@material-ui/core/Link'
-import Grid from '@material-ui/core/Grid'
-import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
+import {
+	Avatar,
+	Button,
+	CssBaseline,
+	TextField,
+	FormControlLabel,
+	Checkbox,
+	Link,
+	Grid,
+	Box,
+	Container,
+} from '@material-ui/core'
+
 import { FiUserPlus } from 'react-icons/fi'
 
 import { Formik, Form, Field } from 'formik'
@@ -18,38 +19,8 @@ import { object, string } from 'yup'
 
 import api from '../../middlewares/api'
 
-function Copyright() {
-	return (
-		<Typography variant='body2' color='textSecondary' align='center'>
-			{'Copyright © '}
-			<Link color='inherit' href='https://material-ui.com/'>
-				Your Website
-			</Link>{' '}
-			{new Date().getFullYear()}
-			{'.'}
-		</Typography>
-	)
-}
-
-const useStyles = makeStyles((theme) => ({
-	paper: {
-		marginTop: theme.spacing(2),
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-	},
-	avatar: {
-		margin: theme.spacing(1),
-		backgroundColor: theme.palette.primary.main,
-	},
-	form: {
-		width: '100%', // Fix IE 11 issue.
-		marginTop: theme.spacing(3),
-	},
-	submit: {
-		margin: theme.spacing(3, 0, 2),
-	},
-}))
+import Copyright from '../../components/Copyright'
+import useStyles from './styles'
 
 export default function SignUp() {
 	const classes = useStyles()
