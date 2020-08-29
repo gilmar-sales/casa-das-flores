2e1e25a6e27428b03aaf6e4652f8ee22c29e366a
gilmarxd <gilmar_custodio_@hotmail.com>
2020-08-29 11:33:26 -0300

    [back-end]: add json web token on authentication

--- back-end/src/controllers/profileController.ts
+++ back-end/src/controllers/profileController.ts
@@ -1,31 +1,38 @@
 import { Request, Response } from 'express'
-import { getConnection, Index } from 'typeorm'
+import { getRepository } from 'typeorm'
 import { User } from '../entity/User'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export default {
 	async read(req: Request, res: Response) {
-		const user = await getConnection('default')
-			.getRepository(User)
-			.createQueryBuilder('user')
-			.where('user.id = :id', { id: req.params.id })
-			.getOne()
+		const repository = getRepository(User)
+		const user = await repository.findOne({ where: { id: req.params.id } })
 
 		return res.send(user)
 	},
 	async authenticate(req: Request, res: Response) {
 		const { email, password } = req.body
 
-		const user = await getConnection()
-			.getRepository(User)
+		const repository = getRepository(User)
+
+		const user = await repository
 			.createQueryBuilder('user')
 			.addSelect('user.password')
 			.where('user.email = :email', { email: email })
 			.getOne()
 
-		if (!user) res.send({ errors: { email: 'Usuário não encontrado' } })
-		else if (!(await bcrypt.compare(password, user.password)))
-			res.send({ errors: { password: 'Senha incorreta' } })
-		else res.send({ id: user.id })
+		if (!user) {
+			return res.send({ errors: { email: 'Usuário não encontrado' } })
+		}
+
+		const isValidPassword = await bcrypt.compare(password, user.password)
+		if (!isValidPassword) {
+			return res.send({ errors: { password: 'Senha incorreta' } })
+		}
+
+		const token = jwt.sign({ id: user.id }, 'secret', { expiresIn: '1d' })
+
+		return res.send({ user, token })
 	},
 }
