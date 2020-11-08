692ad0431497c86d2ee41e4e9a9f402473fb64bd
Gilmar Custodio de Sales <gilmar@MacBook-Pro-de-Gilmar.local>
2020-11-08 11:32:17 -0300

    [back-end]: set snake naming strategy to typeorm

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,14 @@
 import app from './app'
-import { createConnection } from 'typeorm'
+import { createConnection, getConnectionOptions } from 'typeorm'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 
-createConnection()
+getConnectionOptions().then((connectionOptions) => {
+	return createConnection(
+		Object.assign(connectionOptions, {
+			namingStrategy: new SnakeNamingStrategy(),
+		})
+	)
+})
 
 app.listen(3333, () => {
 	console.log(`Server running on http://localhost:3333`)
