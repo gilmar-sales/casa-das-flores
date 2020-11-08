ee8b9bfe2e6abf9686c103312c666710252b38df
Gilmar Custodio de Sales <gilmar@MacBook-Pro-de-Gilmar.local>
2020-11-08 18:14:41 -0300

    [front-end]: test products in home page

--- front-end/src/pages/Home/index.tsx
+++ front-end/src/pages/Home/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { Carousel, Pagination, Row } from 'antd'
+import { Carousel, Col, Pagination, Row } from 'antd'
+
+import ProductCard from '../../components/product/ProductCard'
 
 const contentStyle = {
 	height: '200px',
 	color: '#fff',
 	background: 'black',
+}
+
+const product = {
+	id: 1,
+	name: 'Buquê',
+	description:
+		'Buquê de rosas vermelhas asd as sd as asd asdasdas as das das  sadas asd',
+	unitPrice: 90,
+	width: 12,
+	height: 50,
+	depth: 10,
+	createdDate: 'string',
+	pictures: [{ path: '' }],
+	category: {
+		id: 1,
+		name: 'string',
+		description: 'string',
+		picture: 'string',
+	},
 }
 
 export default function () {
@@ -33,7 +54,25 @@ export default function () {
 					showSizeChanger={false}
 				/>
 			</Row>
-			<Row>No products!</Row>
+			<Row
+				style={{
+					paddingTop: 16,
+					paddingLeft: 8,
+					paddingRight: 8,
+					paddingBottom: 16,
+					margin: 0,
+					maxWidth: '100vw',
+				}}
+				gutter={[16, 16]}
+			>
+				{[...Array(12)].map(() => {
+					return (
+						<Col xs={24} sm={12} md={6} lg={6} xl={4} xxl={2}>
+							<ProductCard product={product}></ProductCard>
+						</Col>
+					)
+				})}
+			</Row>
 			<Row justify='center'>
 				<Pagination
 					defaultCurrent={1}
