2b4b52813f577b4cfc5f4d1950ad23baf3214536
Gilmar <gilmar_custodio_@hotmail.com>
2020-11-11 18:02:03 -0300

    [front-end]: persist name and profile picture on local storage

--- front-end/src/middlewares/auth.ts
+++ front-end/src/middlewares/auth.ts
@@ -1,13 +1,23 @@
 export const TOKEN_KEY = '@casa-das-flores-Token'
+export const NAME_KEY = '@name'
+export const LAST_NAME_KEY = '@lastName'
+export const PROFILE_PICTURE_KEY = '@profile-picture'
 
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY)
 
-export const login = (token: string) => {
-	localStorage.setItem(TOKEN_KEY, token)
+export const login = (data: any) => {
+	console.log(data.user)
+	localStorage.setItem(TOKEN_KEY, data.token)
+	localStorage.setItem(NAME_KEY, data.user.firstName)
+	localStorage.setItem(LAST_NAME_KEY, data.user.lastName)
+	localStorage.setItem(PROFILE_PICTURE_KEY, data.user.profilePicture)
 }
 
 export const logout = () => {
 	localStorage.removeItem(TOKEN_KEY)
+	localStorage.removeItem(NAME_KEY)
+	localStorage.removeItem(LAST_NAME_KEY)
+	localStorage.removeItem(PROFILE_PICTURE_KEY)
 }
