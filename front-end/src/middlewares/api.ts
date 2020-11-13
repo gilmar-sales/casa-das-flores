dcdba4d95f3c2ac1740a82d2bd4d83f844dbeb35
Gilmar <gilmar_custodio_@hotmail.com>
2020-11-13 12:01:37 -0300

    [front-end]: add env vars to api middleware

--- front-end/src/middlewares/api.ts
+++ front-end/src/middlewares/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 import { getToken } from './auth'
 
-const api = axios.create({ baseURL: 'http://192.168.1.2:3333' })
+const port = process.env.PORT || 3333
+const url = process.env.HOST_NAME || `http://localhost:${port}`
+
+const api = axios.create({ baseURL: `${url}/api` })
 
 api.interceptors.request.use(async (config) => {
 	const token = getToken()
