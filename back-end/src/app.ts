1412177fa74fd8cb377105961a44dffb91182b25
Gilmar <gilmar_custodio_@hotmail.com>
2020-11-13 11:28:28 -0300

    [back-end]: redirect root to build

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express'
 import cors from 'cors'
+import path from 'path'
 
 import routes from './routes'
 
@@ -11,6 +12,7 @@ class App {
 
 		this.middlewares()
 		this.routes()
+		this.ui()
 	}
 
 	private middlewares() {
@@ -19,7 +21,17 @@ class App {
 	}
 
 	private routes() {
-		this.express.use(routes)
+		this.express.use('/api', routes)
+	}
+	private ui() {
+		this.express.use(
+			express.static(path.resolve(__dirname, '../../front-end/build'))
+		)
+		this.express.use('*', (request, response) => {
+			response.sendFile(
+				path.resolve(__dirname, '../../front-end/build', 'index.html')
+			)
+		})
 	}
 }
 
