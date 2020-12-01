1ca3df2e8430a59945b051ba24fbe0e40227b707
Gilmar Custodio <gilmar@pop-os.localdomain>
2020-12-01 18:04:34 -0300

    [front-end]: api from lan address

--- front-end/src/middlewares/api.ts
+++ front-end/src/middlewares/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getToken } from './auth'
 
-const api = axios.create({ baseURL: 'http://127.0.0.1:3333' })
+const api = axios.create({ baseURL: 'http://192.168.1.2:3333' })
 
 api.interceptors.request.use(async (config) => {
 	const token = getToken()
