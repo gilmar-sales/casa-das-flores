0753abd391156965172553a8c34d6b013cedae8f
gilmarxd <gilmar_custodio_@hotmail.com>
2021-01-14 10:52:15 -0300

    [front-end]: add profile protected route

---
+++ front-end/src/pages/Profile/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function Profile() {
+	return (
+		<div className='grid grid-cols-6 gap-4 px-4'>
+			{/* Menu */}
+			<div className='col-span-6 sm:col-span-1 rounded-md border py-2'>
+				Menu
+			</div>
+			{/* Profile */}
+			<div className='col-span-6 sm:col-span-5 rounded-md border p-2'>
+				<div className='grid grid-cols-6'>
+					<div className='col-span-6 md:col-span-2'>
+						<div className='flex justify-center items-center rounded-full bg-green-500 text-white h-16 w-16 text-2xl select-none'>
+							GC
+						</div>
+						<div>Gilmar Custodio</div>
+					</div>
+					<div className='col-span-6 md:col-span-4 grid grid-cols-2'>
+						<div className='col-span-2 sm:col-span-1'>Telefone</div>
+						<div className='col-span-2 sm:col-span-1'>Cidade</div>
+						<div className='col-span-2 sm:col-span-2'>Endereço</div>
+					</div>
+				</div>
+				<div className='w-full h-1 border-b' />
+				<div className='grid grid-cols-5 pt-4 gap-4'>
+					<div className='col-span-5 sm:col-span-2 md:col-span-1 flex items-center flex-col '>
+						<span className='text-3xl font-bold'>0</span>
+						<span className='text-center'>Total de Pedidos</span>
+					</div>
+					<div className='col-span-5 sm:col-span-2 md:col-span-1 flex items-center flex-col'>
+						<span className='text-3xl font-bold '>0</span>
+						<span className='text-center'>Pagamentos Pendentes</span>
+					</div>
+					<div className='col-span-5 sm:col-span-2 md:col-span-1 flex items-center flex-col'>
+						<span className='text-3xl font-bold'>0</span>
+						<span className='text-center'>Envios Pendentes</span>
+					</div>
+					<div className='col-span-5 sm:col-span-2 md:col-span-1 flex items-center flex-col'>
+						<span className='text-3xl font-bold'>0</span>
+						<span className='text-center'>Aguardando Entrega</span>
+					</div>
+					<div className='col-span-5 sm:col-span-2 md:col-span-1  flex items-center flex-col'>
+						<span className='text-3xl font-bold'>0</span>
+						<span className='text-center'>Avaliações Pendentes</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -1,32 +1,35 @@
-import React from "react";
+import React from 'react'
 import {
-  BrowserRouter,
-  Switch,
-  Route,
-  RouteProps,
-  Redirect,
-} from "react-router-dom";
+	BrowserRouter,
+	Switch,
+	Route,
+	RouteProps,
+	Redirect,
+} from 'react-router-dom'
 
-import { isAuthenticated } from "./middlewares/auth";
+import { isAuthenticated } from './middlewares/auth'
 
-import NavBar from "./components/NavBar";
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 const ProtectedRoute: React.FC<RouteProps> = (props) => {
-  if (!isAuthenticated()) {
-    return <Redirect to={{ pathname: "/", state: { from: props.location } }} />;
-  } else {
-    return <Route {...props} />;
-  }
-};
+	if (!isAuthenticated()) {
+		return <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+	} else {
+		return <Route {...props} />
+	}
+}
 
 export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <NavBar>
-          <Route path="/" exact />
-        </NavBar>
-      </Switch>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<NavBar>
+					<Route path='/' exact component={Home} />
+					<ProtectedRoute path='/profile' component={Profile} />
+				</NavBar>
+			</Switch>
+		</BrowserRouter>
+	)
 }
