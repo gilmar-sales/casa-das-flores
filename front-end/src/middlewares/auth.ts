b35b126ed3dc0acffffabfa538cf8e1216945129
gilmarxd <gilmar_custodio_@hotmail.com>
2021-01-14 10:53:11 -0300

    [front-end]: remove user data from local storage

--- front-end/src/middlewares/auth.ts
+++ front-end/src/middlewares/auth.ts
@@ -8,16 +8,9 @@ export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null
 export const getToken = () => localStorage.getItem(TOKEN_KEY)
 
 export const login = (data: any) => {
-	console.log(data.user)
 	localStorage.setItem(TOKEN_KEY, data.token)
-	localStorage.setItem(NAME_KEY, data.user.firstName)
-	localStorage.setItem(LAST_NAME_KEY, data.user.lastName)
-	localStorage.setItem(PROFILE_PICTURE_KEY, data.user.profilePicture)
 }
 
 export const logout = () => {
 	localStorage.removeItem(TOKEN_KEY)
-	localStorage.removeItem(NAME_KEY)
-	localStorage.removeItem(LAST_NAME_KEY)
-	localStorage.removeItem(PROFILE_PICTURE_KEY)
 }
