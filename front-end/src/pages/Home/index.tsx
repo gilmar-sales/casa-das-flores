0c9df5c99f092ffb15e2efecbec80fe55a65a07c
gilmarxd <gilmar_custodio_@hotmail.com>
2021-02-09 18:57:06 -0300

    [front-end]: limit width

--- front-end/src/pages/Home/index.tsx
+++ front-end/src/pages/Home/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 	}
 
 	return (
-		<div className='flex flex-row'>
+		<div className='flex flex-col'>
 			{/* Categories */}
 			<div className='hidden md:flex flex-col border-dashed border-r-2 border-green-100 py-2 items-center justify-around'>
 				{categories.map((category, index) => (
