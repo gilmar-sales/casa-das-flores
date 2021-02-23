f0882ab7a329e069c44210d84b243d7e242f4ac1
gilmarxd <gilmar_custodio_@hotmail.com>
2021-02-23 18:42:44 -0300

    [front-end]: remove unnecessary code

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,7 +7,7 @@ import { ShopBagProvider } from './contexts/ShopBagContext'
 
 function App() {
 	return (
-		<div className='App '>
+		<div>
 			<SignProvider>
 				<ShopBagProvider>
 					<Routes />
